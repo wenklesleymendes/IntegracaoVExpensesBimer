f3691135467885548fe6735f6b2a409c3ccb492a
Mendes <mendes@keltecnologia.com.br>
2025-02-24 10:54:33 -0300

    Criando migratins no banco

---
+++ PARS.Inhouse.Systems.Infrastructure/Migrations/20250224133740_202502241032_v5.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PARS.Inhouse.Systems.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202502241032_v5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
